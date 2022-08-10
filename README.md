e96385b6ed29a2f638db2dad2d7d3dbcd6a9a0a3
amerucci <a.merucci@onlineformapro.com>
2022-08-10 15:10:48 +0200

    Initialisation project

---
+++ README.md
@@ -0,0 +1,19 @@
+# conciergerie2022
+
+Realiser un CRUD pour le concierge d'un immeuble.
+
+M. Menard est concierge d'un immeuble situé 2 route de Montaigu à Lons le Saunier.
+En tant que concierge il doit s'occuper de toutes les taches de maintenance dans son bâtiment.
+Il aimerait donc avoir une administration dans laquelle il pourrait saisir ses intervention, avec une date d'intervention, le type d'intervention (exemple : changement ampoule, remplacement serrure) et l'étage concerné.
+Il devra aussi pouvoir depuis celle-ci, pouvoir afficher les intervention d'une certaine date, les interventions réalisées à tel ou tel étage...
+
+
+Il est attendu un rendu en ligne et sur GitHUB + La fourniture d'un MCD + MLD
+
+Ce travail se fera individuellement
+
+Il est attendu un travail propre, fonctionnel et professionnel.
+
+
+
+
---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DashBoard - Le Magellan</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.15.2/dist/css/uikit.min.css">
+</head>
+
+<body>
+    <?php
+    session_start();
+    $_SESSION['nom_user'] = "toto";
+    if (isset($_SESSION['nom_user'])) { ?>
+        <div class="uk-container">
+            <table class="uk-table uk-table-hover uk-table-divider">
+                <thead>
+                    <tr>
+                        <th>Type intervention</th>
+                        <th>Etage intervention</th>
+                        <th>Date intervention</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Table Data</td>
+                        <td>Table Data</td>
+                        <td>Table Data</td>
+                    </tr>
+                    <tr>
+                        <td>Table Data</td>
+                        <td>Table Data</td>
+                        <td>Table Data</td>
+                    </tr>
+                    <tr>
+                        <td>Table Data</td>
+                        <td>Table Data</td>
+                        <td>Table Data</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    <?php
+    } else {
+        header('Location: ./login.php');
+    }
+
+    ?>
+</body>
+
+</html>
